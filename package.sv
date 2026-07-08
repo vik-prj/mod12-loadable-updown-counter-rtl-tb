87b446128db5aa0d470b9a9ec75d2644b6c9fbe0
vik-prj <vik.prj5@gmail.com>
2026-07-08 17:55:04 +0530

    Create package.sv

---
+++ package.sv
@@ -0,0 +1,13 @@
+package pkg;
+
+  `include "trans.sv"
+  `include "generator.sv"
+  `include "write_driver.sv"
+  `include "write_monitor.sv"
+  `include "read_monitor.sv"
+  `include "reference_model.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv"
+
+endpackage
